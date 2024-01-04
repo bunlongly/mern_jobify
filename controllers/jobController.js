c7fea343054e46d640e44120d84f9131032bdb42
LonG <bunlongly03@gmail.com>
2024-01-04 11:55:41 +0700

    Async Errors

--- controllers/jobController.js
+++ controllers/jobController.js
@@ -12,8 +12,7 @@ export const getAllJobs = async (req, res) => {
 };
 
 export const createJob = async (req, res) => {
-  // const { company, position } = req.body;
-  const job = await Job.create(req.body);
+  const job = await Job.create("something");
   res.status(201).json({ job });
 };
 
