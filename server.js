a6bce6287845ee1cd204fcf579669e0dc88d931a
LonG <bunlongly03@gmail.com>
2024-01-07 11:28:22 +0700

    Validation Middleware

--- server.js
+++ server.js
@@ -5,7 +5,8 @@ import express from "express";
 const app = express();
 import morgan from "morgan";
 import mongoose from "mongoose";
-import { body, validationResult } from "express-validator";
+
+import { validateTest } from "./middleware/validationMiddleware.js";
 
 // routers
 import jobRouter from "./routes/jobRouter.js";
@@ -23,28 +24,10 @@ app.get("/", (req, res) => {
   res.send("Hello World");
 });
 
-app.post(
-  "/api/v1/test",
-  [
-    body("name")
-      .notEmpty()
-      .withMessage("name is required")
-      .isLength({ min: 50 })
-      .withMessage("name most be at least 50"),
-  ],
-  (req, res, next) => {
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-      const errorMessages = errors.array().map((error) => error.msg);
-      return res.status(400).json({ errors: errorMessages });
-    }
-    next();
-  },
-  (req, res) => {
-    const { name } = req.body;
-    res.json({ msg: `hello ${name}` });
-  }
-);
+app.post("/api/v1/test", validateTest, (req, res) => {
+  const { name } = req.body;
+  res.json({ msg: `hello ${name}` });
+});
 
 app.use("/api/v1/jobs", jobRouter);
 
