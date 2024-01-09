c492f2881e61230a79bdbb855730d3a268acda40
LonG <bunlongly03@gmail.com>
2024-01-09 16:04:28 +0700

    Validate Login, Login Logic

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,6 +1,7 @@
 import { StatusCodes } from "http-status-codes";
 import User from "../models/UserModel.js";
-import { hashPassword } from "../utils/passwordUtils.js";
+import { comparePassword, hashPassword } from "../utils/passwordUtils.js";
+import { UnauthenticatedError } from "../errors/customError.js";
 
 export const register = async (req, res) => {
   const isFistAccount = (await User.countDocuments()) === 0;
@@ -13,6 +14,28 @@ export const register = async (req, res) => {
   res.status(StatusCodes.CREATED).json({ msg: "user created" });
 };
 
+
+
+// export const login = async (req, res) => {
+//   const user = await User.findOne({ email: req.body.email });
+
+//   const isValidUser =
+//     user && (await comparePassword(req.body.password, user.password));
+
+//   if (!isValidUser) throw new UnauthenticatedError("invalid credentials");
+// };
+
 export const login = async (req, res) => {
-  res.send("login");
+
+  const user = await User.findOne({ email: req.body.email });
+
+  if (!user) throw new UnauthenticatedError("invalid credentials");
+
+  const isPasswordCorrect = await comparePassword(
+    req.body.password,
+    user.password
+  );
+
+  if (!isPasswordCorrect) throw new UnauthenticatedError("invalid credentials");
+  res.send("login route");
 };
