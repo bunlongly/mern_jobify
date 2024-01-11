6ebce700c3ca44ca6b5318e21ad582cfc3cfad8e
LonG <bunlongly03@gmail.com>
2024-01-11 12:07:39 +0700

    User Routes

--- server.js
+++ server.js
@@ -10,6 +10,7 @@ import cookieParser from "cookie-parser";
 // routers
 import jobRouter from "./routes/jobRouter.js";
 import authRouter from "./routes/authRouter.js";
+import userRouter from './routes/userRouter.js'
 
 //middleware
 import errorHandlerMiddleware from "./middleware/errorHandlerMiddleware.js";
@@ -22,11 +23,13 @@ if (process.env.NODE_ENV === "development") {
 app.use(cookieParser());
 app.use(express.json());
 
+
 app.get("/", (req, res) => {
   res.send("Hello World");
 });
 
 app.use("/api/v1/jobs", authenticateUser, jobRouter);
+app.use("/api/v1/users", authenticateUser, userRouter);
 app.use("/api/v1/auth", authRouter);
 
 app.use("*", (req, res) => {
