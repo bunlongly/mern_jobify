3384b607f7bde6a1383fcf7ebdb9218754e6f452
LonG <bunlongly03@gmail.com>
2024-01-12 15:40:22 +0700

    Update User

--- controllers/userController.js
+++ controllers/userController.js
@@ -3,15 +3,19 @@ import User from "../models/UserModel.js";
 import Job from "../models/JobModel.js";
 
 export const getCurrentUser = async (req, res) => {
-    const user = await User.findOne({ _id: req.user.userId });
-    const userWithoutPassword = user.toJSON()
-    res.status(StatusCodes.OK).json({ user: userWithoutPassword });
-  };
+  const user = await User.findOne({ _id: req.user.userId });
+  const userWithoutPassword = user.toJSON();
+  res.status(StatusCodes.OK).json({ user: userWithoutPassword });
+};
 
 export const getApplicationStats = async (req, res) => {
   res.status(StatusCodes.OK).json({ msg: "application stats" });
 };
 
 export const updateUser = async (req, res) => {
+  const obj = { ...req.body };
+  delete obj.password
+  console.log(obj)
+  const updateUser = await User.findByIdAndUpdate(req.user.userId, req.body, obj);
   res.status(StatusCodes.OK).json({ msg: "update user" });
 };
