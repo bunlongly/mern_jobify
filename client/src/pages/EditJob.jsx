93c5c0b3bbc04f261356e7226064a376850ddd26
LonG <bunlongly03@gmail.com>
2024-01-24 15:30:32 +0700

    Route Params

--- client/src/pages/EditJob.jsx
+++ client/src/pages/EditJob.jsx
@@ -1,5 +1,22 @@
-const EditJob = () => {
-    return <h1>EditJob Page</h1>
-}
+import { FormRow, FormRowSelect } from "../components";
+import Wrapper from "../assets/wrappers/DashboardFormPage";
+import { useLoaderData, useParams } from "react-router-dom";
+import { JOB_STATUS, JOB_TYPE } from "../../../utils/constants";
+import { Form, useNavigation, redirect } from "react-router-dom";
+import { toast } from "react-toastify";
+import customFetch from "../utils/customFetch";
 
-export default EditJob
+export const loader = async ({ params }) => {
+  console.log(params);
+  return null;
+};
+export const action = async () => {
+  return null;
+};
+
+const EditJob = () => {
+  const params = useParams();
+  console.log(params);
+  return <h1>EditJob Page</h1>;
+};
+export default EditJob;
