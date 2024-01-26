9b7c093ef28165035fad97f8ce1fa277a905f75b
LonG <bunlongly03@gmail.com>
2024-01-26 11:22:17 +0700

    Admin Page Complete

--- client/src/pages/Admin.jsx
+++ client/src/pages/Admin.jsx
@@ -1,5 +1,26 @@
-const Admin = () => {
-    return <h1>Admin Page</h1>
-}
+import { FaSuitcaseRolling, FaCalendarCheck } from "react-icons/fa";
 
-export default Admin
+import { useLoaderData, redirect } from "react-router-dom";
+import customFetch from "../utils/customFetch";
+import Wrapper from "../assets/wrappers/StatsContainer";
+import { toast } from "react-toastify";
+export const loader = async () => {
+  try {
+    const response = await customFetch.get("/users/admin/app-stats");
+    return response.data;
+  } catch (error) {
+    toast.error("You are not authorized to view this page");
+    return redirect("/dashboard");
+  }
+};
+
+const Admin = () => {
+  const { users, jobs } = useLoaderData();
+
+  return (
+    <Wrapper>
+      <h2>admin page</h2>
+    </Wrapper>
+  );
+};
+export default Admin;
