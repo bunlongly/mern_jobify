ba57e73eb10c5c1551ab02e374099445fce6b127
LonG <bunlongly03@gmail.com>
2024-01-29 14:05:59 +0700

    Stats Route Setup

--- routes/jobRouter.js
+++ routes/jobRouter.js
@@ -6,12 +6,16 @@ import {
   createJob,
   updateJob,
   deleteJob,
+  showStats
 } from "../controllers/jobController.js";
 import { validateJobInput, validateIdParam } from "../middleware/validationMiddleware.js";
 import { checkForTestUser } from "../middleware/authMiddleware.js";
 
 
 router.route("/").get(getAllJobs).post(checkForTestUser, validateJobInput, createJob);
+
+router.route('/stats').get(showStats)
+
 router
 .route("/:id")
   .get(checkForTestUser,  validateIdParam, getJob)
