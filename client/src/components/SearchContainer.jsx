d726ee32c3bfb76bd3451fb30ce1ce04315cccbf
LonG <bunlongly03@gmail.com>
2024-02-01 13:21:39 +0700

    Debounce

--- client/src/components/SearchContainer.jsx
+++ client/src/components/SearchContainer.jsx
@@ -8,21 +8,33 @@ const SearchContainer = () => {
   const { searchValues } = useAllJobsContext();
   const { search, jobStatus, jobType, sort } = searchValues;
   const submit = useSubmit();
+
+  const debounce = (onChange) => {
+    let timeout;
+    return (e) => {
+      const form = e.currentTarget.form;
+      clearTimeout(timeout);
+      timeout = setTimeout(() => {
+        onChange(form);
+      }, 2000);
+    };
+  };
+
   return (
     <Wrapper>
       <Form className="form">
         <h5 className="form-title">search form</h5>
         <div className="form-center">
           {/* search position */}
-
           <FormRow
             type="search"
             name="search"
             defaultValue={search}
-            onChange={(e) => {
-              submit(e.currentTarget.form);
-            }}
+            onChange={debounce((form) => {
+              submit(form);
+            })}
           />
+
           <FormRowSelect
             labelText="job status"
             name="jobStatus"
@@ -49,7 +61,6 @@ const SearchContainer = () => {
               submit(e.currentTarget.form);
             }}
           />
-
           <Link to="/dashboard/all-jobs" className="btn form-btn delete-btn">
             Reset Search Values
           </Link>
