7db0ab09d97d5b256e5f5098fd0bb7ed9d48da35
LonG <bunlongly03@gmail.com>
2024-02-01 14:00:18 +0700

    Pagnitaion

--- client/src/components/JobContainer.jsx
+++ client/src/components/JobContainer.jsx
@@ -1,11 +1,12 @@
 import Job from "./Job";
 import Wrapper from "../assets/wrappers/JobsContainer";
+import PageBtnContainer from '../components/PageBtnContainer'
 
 import { useAllJobsContext } from "../pages/AllJobs";
 
 const JobContainer = () => {
   const { data } = useAllJobsContext();
-  const { jobs } = data;
+  const { jobs, totalJobs, numOfPages } = data;
   if (jobs.length === 0) {
     return (
       <Wrapper>
@@ -16,11 +17,15 @@ const JobContainer = () => {
 
   return (
     <Wrapper>
+      <h5>
+        {totalJobs} job{jobs.length > 1 && "s"} found
+      </h5>
       <div className="jobs">
         {jobs.map((job) => {
           return <Job key={job._id} {...job} />;
         })}
       </div>
+      {numOfPages > 1 && <PageBtnContainer />}
     </Wrapper>
   );
 };
